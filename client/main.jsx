2a6d9e757185ba7842566998858166e50d935e05
nonameable <ddelgado2013@gmail.com>
2017-03-23 03:15:37 -0500

    added date picker, working and registering data into our mongo database

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from '../imports/ui/App.jsx';
 
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,9 +3,30 @@ import { createContainer } from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 import { Transactions } from '../api/transactions.js';
 import Transaction from './Transaction.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import DatePicker from 'material-ui/DatePicker';
 
 // App component - represents the whole app
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      controlledDate: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event, date) {
+    this.setState({
+      controlledDate: date,
+    });
+  };
 
   renderTransactions() {
     return this.props.transactions.map((transaction) => (
@@ -21,14 +42,15 @@ class App extends Component {
     const description = ReactDOM.findDOMNode(this.refs.description).value.trim();
     const account = Number(ReactDOM.findDOMNode(this.refs.account).value.trim());
     const io = ReactDOM.findDOMNode(this.refs.io).value.trim();
-    //console.log("inserted! ", amount+ ":"+ account + ":" + description+ ":"+ io+ ":");
+    const date = this.state.controlledDate;
+    console.log("inserted! ", amount+ ":"+ account + ":" + description+ ":"+ io+ ":" + date);
 
     Transactions.insert({
       amount: amount,
       account: account,
       description: description,
       io: io,
-      date: new Date(), // current time
+      date: date // date selected by the user
     });
 
 
@@ -40,61 +62,71 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div className="container">
-          <header>
-            <h1>Kipcount</h1>
-            <form className="new-transaction" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="amount"
-              placeholder="Type amount"
-            />
-            <select
-              ref="account"
-              name="Account">
-              <option value="1">Cash</option>
-              <option value="2">Bank</option>
-            </select>
+      <MuiThemeProvider>
+        <div className="all">
+          <div className="container">
+            <header>
+              <h1>Kipcount</h1>
+              <form className="new-transaction" onSubmit={this.handleSubmit.bind(this)} >
+              <input
+                type="text"
+                ref="amount"
+                placeholder="Type amount"
+              />
+              <select
+                ref="account"
+                name="Account">
+                <option value="1">Cash</option>
+                <option value="2">Bank</option>
+              </select>
 
-            <select
-              ref="io"
-              name="Income or expense">
-              <option value="in">Income</option>
-              <option value="out">Expense</option>
-            </select>
-            <textarea
-              name="message"
-              rows="4"
-              cols="20"
-              ref="description"
-              placeholder="Describe the transaction"
-              >
+              <select
+                ref="io"
+                name="Income or expense">
+                <option value="in">Income</option>
+                <option value="out">Expense</option>
+              </select>
+              <textarea
+                name="message"
+                rows="4"
+                cols="20"
+                ref="description"
+                placeholder="Describe the transaction"
+                >
 
-            </textarea>
-            <input type="submit" value="Submit" />
-          </form>
+              </textarea>
+              <DatePicker
+                      hintText="Controlled Date Input"
+                      value={this.state.controlledDate}
+                      onChange={this.handleChange}
+              />
 
-          </header>
+             <input type="submit" value="Submit" />
+            </form>
 
-          <ul>
-            {this.renderTransactions()}
-          </ul>
+            </header>
 
-          <div className="summary">
-            Here we will have the summary of the current transactions
+            <ul>
+              {this.renderTransactions()}
+            </ul>
+
+            <div className="summary">
+              Here we will have the summary of the current transactions
+            </div>
           </div>
-        </div>
 
-        <div className="total">
-          Total panel with the general balance of the person
-        </div>
+          <div className="total">
+            Total panel with the general balance of the person
+          </div>
 
-        <div className="accounts">
-          accounts information
-        </div>
+          <div className="accounts">
+            accounts information
+          </div>
 
-      </div>
+        </div>
+        </MuiThemeProvider>
+
+
 
     );
   }
