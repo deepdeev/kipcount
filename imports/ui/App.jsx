c2d83cf9f892b20f27eb62edc99144471417140f
LuisMesa <la.mesa10@uniandes.edu.co>
2017-03-28 04:03:49 -0500

    Landing page added, improvments in styles in all the page and custom login

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -28,6 +28,7 @@ class App extends Component {
 
     this.changeDateFilter = this.changeDateFilter.bind(this);
     this.filteredTransactions = this.filteredTransactions.bind(this);
+
   }
 
   filteredTransactions()
@@ -166,7 +167,7 @@ class App extends Component {
         description: description,
         io: io,
         date: date // date selected by the user
-      }
+      };
       Meteor.call('transactions.insert', transaction);
       console.log("inserted! ", amount + ":" + account + ":" + description + ":" + io + ":" + date);
     }
@@ -246,18 +247,17 @@ class App extends Component {
   {
     return (
         <div>
-            <div className="row temporalNavBar">
-              <div className="col-md-2"></div>
-              <div className="col-md-6">
-                <h1 className="navBarTitle">KipCount</h1>
-              </div>
-              <div className="col-md-3">
-                <AccountsUIWrapper/>
-              </div>
-              <div className="col-md-1"></div>
+          <div className="row temporalNavBar">
+            <div className="col-md-2"></div>
+            <div className="col-md-7">
+              <h1 className="navBarTitle">KipCount</h1>
+            </div>
+            <div className="col-md-1 marginTop">
+              <AccountsUIWrapper/>
+            </div>
+            <div className="col-md-2"></div>
           </div>
           <div className="container">
-
             { this.props.currentUser ?
                 <div className="row principal">
                   <div className="col-md-3">
@@ -373,7 +373,63 @@ class App extends Component {
                       </div>
                     </div>
                   </div>
-                </div> : ''
+                </div>
+                :
+                <div className="row loginPanel">
+                  <div className="background-image"></div>
+
+
+                  <div className="content">
+                    <div className="container">
+                      <div className="row">
+
+
+                        <div className="col-md-7 inspiringPhrase">
+                          <h1>Keep count of your income and expenses all in a single place!</h1>
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="col-md-3">
+                          <div className="loginBox">
+                            <form>
+                              <div className="form-group">
+                                <div id="login-username-label-and-input">
+                                  <label id="login-username-label" htmlFor="login-username">
+                                    <h1>Username</h1>
+                                  </label>
+                                  <input className="form-control" id="login-username2" type="text"></input>
+                                </div>
+                                <div id="login-password-label-and-input">
+                                  <label id="login-password-label" htmlFor="login-password">
+                                    <h1>Password</h1>
+                                  </label>
+                                  <input className="form-control" id="login-password2" type="password"></input>
+                                </div>
+                                <span className="login-button login-button-form-submit" id="login-buttons-password2">
+                          <span className="btn btn-success" onClick={() =>
+                          {
+                            if (document.getElementById('login-sign-in-link'))
+                            {
+                              document.getElementById('login-sign-in-link').click();
+                              setTimeout(() =>
+                              {
+                                document.getElementById('login-username').value = document.getElementById('login-username2').value;
+                                document.getElementById('login-password').value = document.getElementById('login-password2').value;
+                                document.getElementById('login-buttons-password').click();
+                              }, 200)
+                            }
+                          }}>Sign in</span>
+                        </span>
+                                <span className="additional-link-container">
+                          <button id="signup-link" className="btn btn-info additional-link">Create account</button>
+                        </span>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             }
 
           </div>
