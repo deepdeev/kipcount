1bd08f22f429323ee6271c541b7efb8e75170b55
nonameable <ddelgado2013@gmail.com>
2017-03-31 23:34:20 -0500

    changes to handle accounts ui with React

--- imports/api/transactions.js
+++ imports/api/transactions.js
@@ -3,6 +3,45 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 export const Transactions = new Mongo.Collection('transactions');
 
+if (Meteor.isClient) {
+  Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+  });
+  Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var usernameVar = event.target.registerUsername.value;
+        var passwordVar = event.target.registerPassword.value;
+        Accounts.createUser({
+            username: usernameVar,
+            password: passwordVar
+        });
+    }
+  },
+  {
+    'click #loginBB': function(event){
+        event.preventDefault();
+        console.log("holle");
+        var usernameVar = event.target.registerUsername.value;
+        var passwordVar = event.target.registerPassword.value;
+        Meteor.loginWithPassword(usernameVar, passwordVar);
+
+    }
+  }
+);
+  Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var usernameVar = event.target.loginUsername.value;
+        var passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(usernameVar, passwordVar);
+    }
+  });
+}
+
 
 if (Meteor.isServer) {
   // This code only runs on the server
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,6 +11,8 @@ import CustomDateFilter from './CustomDateFilter';
 import CustomCheckBox from './CustomCheckBox';
 
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+import InsideAccountsUIWrapper from './InsideAccountsUIWrapper.jsx';
+import OutsideAccountsUIWrapper from './OutsideAccountsUIWrapper.jsx';
 
 
 // App component - represents the whole app
@@ -253,7 +255,7 @@ class App extends Component {
               <h1 className="navBarTitle">KipCount</h1>
             </div>
             <div className="col-md-1 marginTop">
-              <AccountsUIWrapper/>
+              <OutsideAccountsUIWrapper/>
             </div>
             <div className="col-md-2"></div>
           </div>
@@ -396,40 +398,7 @@ class App extends Component {
                         <div className="col-md-1"></div>
                         <div className="col-md-3">
                           <div className="loginBox">
-                            <form>
-                              <div className="form-group">
-                                <div id="login-username-label-and-input">
-                                  <label id="login-username-label" htmlFor="login-username">
-                                    <h1>Username</h1>
-                                  </label>
-                                  <input className="form-control" id="login-username2" type="text"></input>
-                                </div>
-                                <div id="login-password-label-and-input">
-                                  <label id="login-password-label" htmlFor="login-password">
-                                    <h1>Password</h1>
-                                  </label>
-                                  <input className="form-control" id="login-password2" type="password"></input>
-                                </div>
-                                <span className="login-button login-button-form-submit" id="login-buttons-password2">
-                          <span className="btn btn-success" onClick={() =>
-                          {
-                            if (document.getElementById('login-sign-in-link'))
-                            {
-                              document.getElementById('login-sign-in-link').click();
-                              setTimeout(() =>
-                              {
-                                document.getElementById('login-username').value = document.getElementById('login-username2').value;
-                                document.getElementById('login-password').value = document.getElementById('login-password2').value;
-                                document.getElementById('login-buttons-password').click();
-                              }, 200)
-                            }
-                          }}>Sign in</span>
-                        </span>
-                                <span className="additional-link-container">
-                          <button id="signup-link" className="btn btn-info additional-link">Create account</button>
-                        </span>
-                              </div>
-                            </form>
+                            <InsideAccountsUIWrapper/>
                           </div>
                         </div>
                       </div>
